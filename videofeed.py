11549ab4c29d90c8b99955340d040b3c1043b287
abhipanda4 <abhipanda03@gmail.com>
2018-11-12 16:29:39 +0530

    minor changes

--- videofeed.py
+++ videofeed.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 import io
 from PIL import Image
 
@@ -16,22 +16,22 @@ class VideoFeed:
         ret_val, img = self.cam.read()
         c = cv2.waitKey(1)
 
-        cv2.imshow('my webcam', img)
+        # cv2.imshow('my webcam', img)
         cv2_im = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         pil_im = Image.fromarray(cv2_im)
         b = io.BytesIO()
-        pil_im.save(b, 'png')
+        pil_im.save(b, 'jpeg')
         im_bytes = b.getvalue()
         return im_bytes
 
     def set_frame(self, frame_bytes):
         pil_bytes = io.BytesIO(frame_bytes)
         pil_image = Image.open(pil_bytes)
-        cv_image = cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
+        cv_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
         cv2.imshow(self.name, cv_image)
 
 if __name__=="__main__":
-    vf = VideoFeed(1,"test",1)
+    vf = VideoFeed("MAIN", 1)
     while 1:
         m = vf.get_frame()
         vf.set_frame(m)
